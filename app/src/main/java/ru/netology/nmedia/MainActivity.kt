3a8ad4df3f00e33a4f77bf9f0ff566812d53da40
Nikolay Petrenko <nikolosin73@gmail.com>
2024-09-09 15:05:08 +0300

    mvvm

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -2,6 +2,7 @@ package ru.netology.nmedia
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -17,34 +18,30 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val bilding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(bilding.root)
-        with(bilding) {
-            author.text = post.author
-            published.text = post.published
-            content.text = post.content
-            countLike.text = formatCount(post.countLike)
-            countShare.text = formatCount(post.countShare)
-            countViews.text = formatCount(post.views)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-            favorite.setOnClickListener {
-                post.likeByMe = !post.likeByMe
-                if (post.likeByMe) {
-                    favorite.setImageResource(R.drawable.baseline_favorite_true_24)
-                    post.countLike++
-                } else {
-                    favorite.setImageResource(R.drawable.baseline_favorite_24)
-                    post.countLike--
-                }
+        val viewModel: PostViewModel by viewModels()
+        viewModel.data.observe(this) { post ->
+            with(binding) {
+                author.text = post.author
+                published.text = post.published
+                content.text = post.content
+                favorite.setImageResource(
+                    if (post.likeByMe) R.drawable.baseline_favorite_true_24 else R.drawable.baseline_favorite_24
+                )
                 countLike.text = formatCount(post.countLike)
-            }
-
-            share.setOnClickListener {
-                post.countShare++
                 countShare.text = formatCount(post.countShare)
+                countViews.text = formatCount(post.views)
             }
-
         }
-    }
 
+        binding.favorite.setOnClickListener {
+            viewModel.like()
+        }
+        binding.share.setOnClickListener {
+            viewModel.share()
+        }
+
+    }
 }
--- app/src/main/java/ru/netology/nmedia/Post.kt
+++ app/src/main/java/ru/netology/nmedia/Post.kt
@@ -2,24 +2,15 @@ package ru.netology.nmedia
 
 import kotlin.math.floor
 
-class Post(
+data class Post(
     val id: Int,
     val author: String,
     val content: String,
     val published: String,
-    var countLike: Int,
-    var countShare: Int,
-    var views: Int,
-    var likeByMe: Boolean = false
-)
-
-val post = Post(
-    1, "Нетология. Университет интернет профессий",
-    "<Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
-    "4 сентября в 18:00",
-    5,
-    2,
-    10
+    val countLike: Int,
+    val countShare: Int,
+    val views: Int,
+    val likeByMe: Boolean = false
 )
 
 fun formatCount(count: Int): String {
